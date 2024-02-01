5f3c7e825b83e199e353d4eaea2ce236b05ecb36
alekcangp <40195312+alekcangp@users.noreply.github.com>
2024-02-02 01:16:23 +0600

    Update chat.mjs

--- chat.mjs
+++ chat.mjs
@@ -1,8 +1,6 @@
 import OpenAI from "openai";
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+const openai = new OpenAI();
 
 
 import TelegramBot from "node-telegram-bot-api"
